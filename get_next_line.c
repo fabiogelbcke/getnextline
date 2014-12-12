5120d261eeff3048adb1f4c66d70a24490b00fd0
Fabio Gelbcke <dietbacon@Fabios-MacBook-Pro.local>
2014-12-12 15:24:55 +0100

    not handling the huge files but fine otherwise

--- get_next_line.c
+++ get_next_line.c
@@ -12,32 +12,104 @@
 
 #include "get_next_line.h"
 #include "libft/libft.h"
+#include <stdio.h>
 
-void *ft_realloc(void *ptr, size_t size){
-  static size_t size;
-  void *dst;
+char *ft_realloc(char *ptr, size_t size)
+{
+  char *dst;
 
   if (!ptr)
-    return NULL;
-  if (!size)
-    size = 0;
-  size += BUFF_SIZE;
-  dst = (void*)malloc(size);
+    return (NULL);
+  dst = (char*)malloc((size + 1) * sizeof(char));
   if (!dst)
-    return NULL;
+    {
+      printf("realloc failed");
+	return NULL;
+    }
   ft_memcpy(dst, ptr, size - BUFF_SIZE);
-  free(ptr);
-  return dst;
+  return (dst);
 }
 
-int get_next_line(const int fs, char **line)
+void ft_strccat(char *dst, const char *src, char c, int readbytes)
 {
   int i;
-  char *ptr;
-  size_t size;
-
-
-  while( )
+  int dstsize;
+  
+  i = 0;
+  dst += ft_strlen(dst);
+  if (!ft_memchr(src, c, readbytes))
     {
+      ft_memcpy(dst, src, readbytes);
+      dst += readbytes;
+      *dst = '\0';
     }
 }
+
+int get_next_line(const int fd, char **line)
+{
+  char buff[BUFF_SIZE];
+  size_t size;
+  int i;
+  int j;
+
+
+  if(fd < 0)
+    return -1;
+  size = BUFF_SIZE;
+  if(!line || !*line)
+    return -1;
+  *line = "";
+  while(1)
+    {
+      *line = ft_realloc(*line, size);
+      if(!(*line))
+	{
+	  printf("aqui\n");
+	  return -1;
+	}
+      i = 0;
+      while(i < BUFF_SIZE)
+	{
+	  if(read(fd, &(buff[i]), 1) != 1 || buff[i] == '\n')
+	    break;
+	  i++;
+	}
+      j = (buff[i] && buff[i] == '\n') ? i : i + 1;
+      //    while(j < BUFF_SIZE)
+      //	buff[j++] = '\0';
+      ft_strccat(*line, buff, '\n', i);
+      if (buff[i] == '\n')
+	  return 1; 
+      if (i != BUFF_SIZE)
+	{
+	  return 0;
+	}
+      size += BUFF_SIZE;
+    }
+  return -1;
+}
+/*
+int main(void)
+{
+  int fp = open("testfile", O_RDONLY);
+  char *oi;
+  get_next_line(fp, &oi);
+    printf("%s\n", oi);
+  return 0;
+}
+
+int main(void)
+{
+  char *a = malloc (30);
+  char b[] = "heey";
+
+  a[0] = 'u';
+  a[1] = 'a';
+  a[2] = 'b';
+  a[3] = '\0';
+  printf("string = %s\n", a);
+  ft_strccat(a, b, '\n', 4);
+  printf("%s", a);
+  return 0;
+}
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -14,9 +14,13 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#define BUFF_SIZE 32
+#define BUFF_SIZE 1
 
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include "libft/libft.h"
 
 int		get_next_line(int fd, char **line);
 
